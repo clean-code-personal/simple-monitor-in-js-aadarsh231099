7107afbdef5a81e7134d78753587a415c30d29c2
Aadarsh <aadarshhanu23@gmail.com>
2023-02-09 15:31:16 +0530

    trying to reduce complexity

--- batteryCheck.js
+++ batteryCheck.js
@@ -3,15 +3,9 @@ const checkSoc=require('./checkSoc');
 const checkChargeRate=require('./checkCharge');
 
 function batteryIsOk(temperature, soc, charge_rate) {
-     if(checkTemperature(temperature) &&
+     return (checkTemperature(temperature) &&
            checkSoc(soc) &&
-           checkChargeRate(charge_rate))
-           {
-            console.log('Everything is Fine!');
-            return true; 
-           }
-     else
-     return false;
+           checkChargeRate(charge_rate));
 
 }
 
