4ce142eea5a31212b7a2bab225655ab5c97d0054
Aadarsh <aadarshhanu23@gmail.com>
2023-02-11 12:24:27 +0530

    added extension 1 and corresponding tests.

--- check.js
+++ check.js
@@ -1,9 +1,17 @@
-function check(input, min, max, criteria) {
+function isValid(input, min, max, criteria, warningTolerance) {
     if (input < min || input > max) {
         console.log(`${criteria} is out of range!`);
         return false;
     }
+    else if (input < min + warningTolerance) {
+        console.log(`Warning: ${criteria} is approaching lower limit!`);
+        return true;
+    }
+    else if ( input > max - warningTolerance) {
+        console.log(`Warning: ${criteria} is approaching upper limit!`);
+        return true;
+    }
     return true;
 }
 
-module.exports=check;
+module.exports=isValid;
--- inputFun.js
+++ inputFun.js
@@ -1,9 +1,15 @@
-const check = require('./check');
+const isValid = require('./check');
+
 
 function batteryIsOk(temperature, soc, charge_rate) {
-    return check(temperature, 0, 45, 'Temperature') &&
-           check(soc, 20, 80, 'State of Charge') &&
-           check(charge_rate, 0, 0.8, 'Charge rate');
+
+    const ToleranceSoc = 0.05 * 80;
+    const ToleranceTemperature = 0.05 * 45;
+    const ToleranceChargeRate = 0.05 * 0.8;
+
+    return isValid(temperature, 0, 45, 'Temperature',ToleranceTemperature) &&
+           isValid(soc, 20, 80, 'State of Charge',ToleranceSoc) &&
+           isValid(charge_rate, 0, 0.8, 'Charge rate',ToleranceChargeRate);
 }
 
 
